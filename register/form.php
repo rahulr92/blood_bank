e16511b5fb119c27dec735a54912054be2c3fffa
Rahul <rahulr92@gmail.com>
2012-02-03 03:26:50 +0530

    Completed registration form along with DB

--- register/form.php
+++ register/form.php
@@ -48,7 +48,7 @@
 					
 					<!--.....password....-->
 					<li id="li_3" >
-						<label class="description" for="element_3">password: </label>
+						<label class="description" for="element_3">Password: </label>
 						<div>
 							<input id="element_3" name="passwd" class="element text medium" type="password" maxlength="20" value=""/> 
 						</div> 
@@ -57,7 +57,7 @@
 					
 					<!--....confirm password.....-->
 					<li id="li_4" >
-						<label class="description" for="element_4">confirm password </label>
+						<label class="description" for="element_4">Confirm password </label>
 						<div>
 							<input id="element_4" name="cpasswd" class="element text medium" type="password" maxlength="20" value=""/> 
 						</div> 
@@ -97,36 +97,49 @@
 					
 					<!--.....gender....-->
 					<li id="li_10" >
-						<label class="description" for="element_10">gender </label>
+						<label class="description" for="element_10">Gender </label>
 						<span>
-							<input id="element_10_1" name="gender" class="element checkbox" type="radio"  />
+							<input id="element_10_1" name="gender" class="element checkbox" type="radio" value="male" />
 							<label class="choice" for="element_10_1">male</label>
-							<input id="element_10_2" name="gender" class="element checkbox" type="radio" />
+							<input id="element_10_2" name="gender" class="element checkbox" type="radio" value="female" />
 							<label class="choice" for="element_10_2">female</label>
 						</span> 
 					</li>
 					
 					<!--....bloodgroup.....-->
 					<li id="li_11" >
-						<label class="description" for="element_11">blood group </label>
+						<label class="description" for="element_11">Blood group </label>
 						<div>
 							<select class="element select medium" id="element_11" name="bgroup"> 
 							<option value="" selected="selected"></option>
-							<option value="1" >A+</option>
-							<option value="2" >A-</option>
-							<option value="3" >B+</option>
-                                                        <option value="4" >B-</option>
-                                                        <option value="5" >0+</option>
-                                                        <option value="6" >O-</option>
-                                                        <option value="7" >AB+</option>
-                                                        <option value="8" >AB-</option>
+							<option value="A+" >A+</option>
+							<option value="A-" >A-</option>
+							<option value="B+" >B+</option>
+                                                        <option value="B-" >B-</option>
+                                                        <option value="O+" >O+</option>
+                                                        <option value="O-" >O-</option>
+                                                        <option value="AB+" >AB+</option>
+                                                        <option value="AB-" >AB-</option>
 							</select>
 						</div> 
+					</li>
+                    <!--....address.....-->
+					<li id="li_6" >
+						<label class="description" for="element_6">Locality </label>
+						<div>
+						<label for="element_14_1">Area</label>   <input id="element_14_1" name="area" class="element text small" type="" maxlength="20" value=""/> <br />
+						   <label for="element_14_2">PIN code</label> <input id="element_14_2" name="pin" class="element text small" type="text" maxlength="20" value=""/> <br />
+						 <label for="element_14_3">City</label>	     <input id="element_14_3" name="city" class="element text small" type="text" maxlength="20" value=""/> <br />
+							<label for="element_14_4">State</label>     <select name="state" class="element medium">
+        <option value="" selected="selected"> --- Select State ---</option>
+							<option value="INAN"> Andaman Nicobar			</option><option value="INAP"> Andhra Pradesh			</option><option value="INAR"> Arunachal Pradesh			</option><option value="INAS"> Assam			</option><option value="INBI"> Bihar			</option><option value="INCH"> Chandigarh			</option><option value="INCT"> Chhattisgarh			</option><option value="INDA"> Dadra and Nagar Haveli			</option><option value="INDD"> Daman and Diu			</option><option value="INDL"> Delhi			</option><option value="INGO"> Goa			</option><option value="INGU"> Gujarat			</option><option value="INHA"> Haryana			</option><option value="INHP"> Himachal Pradesh			</option><option value="INJK"> Jammu and Kashmir			</option><option value="INJH"> Jharkhand			</option><option value="INKA"> Karnataka			</option><option value="INKR"> Kerala			</option><option value="INLA"> Lakshadweep			</option><option value="INMP"> Madhya Pradesh			</option><option value="INMH"> Maharashtra			</option><option value="INMA"> Manipur			</option><option value="INME"> Meghalaya			</option><option value="INMI"> Mizoram			</option><option value="INNA"> Nagaland			</option><option value="INOR"> Orissa			</option><option value="INPY"> Pondicherry			</option><option value="INPB"> Punjab			</option><option value="INRJ"> Rajasthan			</option><option value="INSI"> Sikkim			</option><option value="INTN"> Tamil Nadu			</option><option value="INTR"> Tripura			</option><option value="INUP"> Uttar Pradesh			</option><option value="INUT"> Uttaranchal			</option><option value="INWB"> West Bengal					</option>
+         </select>
+						</div>
 					</li>
 					
 					<!--....weight.....-->
 					<li id="li_6" >
-						<label class="description" for="element_6">weight </label>
+						<label class="description" for="element_6">Weight </label>
 						<div>
 							<input id="element_6" name="weight" class="element text small" type="text" maxlength="3" value=""/> 
 						</div>
@@ -135,7 +148,7 @@
 					
 					<!--....mobilenumber.....-->
 					<li id="li_7" >
-						<label class="description" for="element_7">mobile number </label>
+						<label class="description" for="element_7">Mobile number </label>
 						<div>
 							<input id="element_7" name="mnum" class="element text medium" type="text" maxlength="10" value=""/> 
 						</div> 
@@ -202,7 +215,7 @@
 		
 		<!--....footer.....-->
 		<div id="footer">
-			powered by<a href="http://www.ieee.org">IEEE</a>
+			Powered by<a href="http://www.ieee.org">IEEE</a>
 		</div>
 		<!--....footer ends here.....-->
 		
--- register/success.php
+++ register/success.php
@@ -1,3 +1,25 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Registration Successful</title>
+<!--<link href="file:///C|/wamp/www/rahul/style.css" rel="stylesheet" type="text/css" />
+--></head>
+
+<body>
 <?php
-echo "registration successful";
+$dob= $_POST['dd']." ".$_POST['mm']." ".$_POST['yy'];
+$dntn_date= $_POST['ldd']." ".$_POST['lmm']." ".$_POST['lyy'];
+$con = new mysqli("localhost","root","","blood_db");
+$stmt= $con->prepare("insert into register(name,uname,pswd,dob,gender,blood_group,area,pin,city,state,weight,mob_no,dontn_date,email) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+$stmt->bind_param("ssssssssssssss",$_POST['name'],$_POST['uname'],$_POST['passwd'],$dob,$_POST['gender'],$_POST['bgroup'],$_POST['area'],$_POST['pin'],$_POST['city'],$_POST['state'],$_POST['weight'],$_POST['mnum'],$dntn_date,$_POST['emailid']);
+$stmt->execute();
+echo "Account has been successfully registered.";
 ?>
+<br />
+<a href="../index.php">
+ Home
+</a>
+</body>
+</html>
